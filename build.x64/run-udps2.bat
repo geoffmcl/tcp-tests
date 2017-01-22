9ed90b3017490880f7d40030b0388384f47462bf
Geoff McLane <ubuntu@geoffair.info>
2017-01-22 17:57:59 +0100

    Add convenient udp 2 run bats

---
+++ build.x64/run-udps2.bat
@@ -0,0 +1,20 @@
+@setlocal
+@set TMPEXE=Release\udpserver2.exe
+@if NOT EXIST %TMPEXE% goto NOEXE
+@set TMPCMD=
+:RPT
+@if "%~1x" == "x" goto GOTCMD
+@set TMPCMD=%TMPCMD% %1
+@shift
+@goto RPT
+:GOTCMD
+
+%TMPEXE% %TMPCMD%
+
+@goto END
+
+:NOEXE
+@echo ERROR: Can NOT locate EXE %TMPEXE%! Check name, location, and FIX ME %0
+@goto END
+
+:END
