c6f2d9d8a986a56ded7c10eaf60815fa7df17b74
Geoff McLane <ubuntu@geoffair.info>
2017-01-24 02:05:18 +0100

    WIP: Add to upd-send.cxx

--- src/udp-send.cxx
+++ src/udp-send.cxx
@@ -10,6 +10,8 @@
 #include <string.h> // for strdup(), ...
 // other includes
 
+#include "winsockerr.cxx"
+
 static const char *module = "udp-send";
 
 static const char *usr_input = 0;
@@ -62,15 +64,44 @@ int parse_args( int argc, char **argv )
     return 1;
 }
 
+int do_test()
+{
+    int iret = 1;
+    SOCKET sd = 0;
+
+    /* The socket() function returns a socket */
+    /* get a socket descriptor */
+    sd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (SERROR(sd)) {
+        PERROR("udp-send: socket() error");
+        iret = 1;
+        goto Exit;
+    }
+    printf("%s: Opened socket %d - Begin %s\n", module, (int)sd, get_datetime_str());
+
+
+Exit:
+    if (sd && !SERROR(sd)) {
+        printf("%s: Close socket %d - End %s\n", module, (int)sd, get_datetime_str());
+        SCLOSE(sd);
+    }
+    return iret;
+}
+
 // main() OS entry
 int main( int argc, char **argv )
 {
     int iret = 0;
     iret = parse_args(argc,argv);
-    if (iret)
+    if (iret) {
+        if (iret == 2)
+            iret = 0;
         return iret;
-
-    // TODO: actions of app
+    }
+    
+    sock_init();
+    iret = do_test(); // actions of app
+    sock_end();
 
     return iret;
 }
